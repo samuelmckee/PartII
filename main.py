174a23ad2fb5030c1641ee47a89f61b3d0ba26b9
Sam McKee <samuel.mckee98@gmail.com>
2018-10-10 22:05:45 +0100

    Refactored code

--- main.py
+++ main.py
@@ -1,57 +1,10 @@
 from sys import argv
 from os import listdir
 from numpy import *
+from scipy.optimize import 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import numpy as np
 
-def get_theta(filename):
-    theta = filename.split("theta")[1][:-4]
-    return float(theta)
-
-
-def get_r(filename):
-    r = filename.split("theta")[0].split("r")[-1]
-    return float(r)
-
-
-def get_energy(file_path):
-    f = open(file_path, "r")
-    for line in f:
-        if "SCF Done" in line:
-            l = line.split()
-            return float(l[4])
-
-def get_data(filename, dir):
-    try:
-        r = get_r(filename)
-        t = get_theta(filename)
-        e = get_energy(dir + "/" + filename)
-        return (r,t,e)
-    except:
-        print("Failed to get data point for " + filename)
-
-def fit_data(data):
-    r_eq, t_eq, e_eq = min(data, key = lambda tup: tup[2])
-    print(r_eq)
-    print(t_eq)
-    print(e_eq)
-
-def plot(data):
-    #Format data so it can be used by matplotlib
-    es = { (r,t): e for (r,t,e) in data }
-
-    rs, ts, _ = zip(*data)    
-    rs = sorted(rs)
-    ts = sorted(ts)
-
-    R,T = meshgrid(rs,ts)
-    E = np.array([ np.array([es[(r,t)] for r in rs]) for t in ts])
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.contour(R,T,E)
-    plt.show()
 
 def main():
     try:
@@ -70,5 +23,38 @@ def main():
 
     fit_data(data)
     plot(data)
+
+
+# Functions for importing data from files
+def get_theta(filename):
+    theta = filename.split("theta")[1][:-4]
+    return float(theta)
+
+
+def get_r(filename):
+    r = filename.split("theta")[0].split("r")[-1]
+    return float(r)
+
+
+def get_energy(file_path):
+    f = open(file_path, "r")
+    for line in f:
+        if "SCF Done" in line:
+            l = line.split()
+            return float(l[4])
+
+
+def get_data(filename, dir):
+    try:
+        r = get_r(filename)
+        t = get_theta(filename)
+        e = get_energy(dir + "/" + filename)
+        return (r,t,e)
+    except:
+        print("Failed to get data point for " + filename)
+
+
+#Functions for processing data
+
 
 main()
