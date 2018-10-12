8db4d972b95ccfe5bf4ba31c3e4fb4ceb3fa247b
Sam McKee <samuel.mckee98@gmail.com>
2018-10-12 16:12:44 +0100

    Formatted plot

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from sys import argv
 from os import listdir
 from datetime import datetime
+from scipy.interpolate import griddata
+from numpy import *
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 
 def main():
@@ -11,7 +14,7 @@ def main():
         dir = argv[1]
     except IndexError:
         print('Please enter directory containing output files as command '
-                            'line argument.')
+            'line argument.')
         return #Exit program
 
     file_list = [str for str in listdir(dir) if str.endswith('.out')]
@@ -21,12 +24,12 @@ def main():
         return #Exit program
 
     data = [get_data(filename, dir) for filename
-                in file_list if get_data(filename, dir) != None]
+        in file_list if get_data(filename, dir) != None]
 
     print('Loaded ' + str(len(data)) + ' files')
 
     dt = datetime.now()
-    out = '%d-%d-%d_%d:%d:%d' % (dt.year,
+    out = '%d-%02d-%02d_%02d:%02d:%02d' % (dt.year,
                                  dt.month,
                                  dt.day,
                                  dt.hour,
@@ -64,18 +67,27 @@ def get_data(filename, dir):
         print('Failed to get data point for ' + filename)
 
 
-#Functions for processing data
+# Functions for processing data
 def plot(data,out):
-    R,T,E = zip(*data)  #Unzip data so it can be used by trisurf
+    # Prepare points to be plotted
+    # Values are interpolated to give smoother colour gradient on plot
+    r,t,e = zip(*data)
+    r_space = linspace(min(r),max(r),100)
+    t_space = linspace(min(t),max(t),100)
+    grid_r, grid_t = meshgrid(r_space, t_space)
+    grid_e = griddata((r, t), e, (grid_r, grid_t), method='cubic')
 
+    #Plot points and save figure
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.view_init(10, 200)
-    ax.plot_trisurf(R,T,E, cmap='autumn')
+    ax.view_init(30,285)
     ax.set_xlabel('r / Angstroms')
     ax.set_ylabel('theta / degrees')
     ax.set_zlabel('Energy / Hartrees')
-    plt.savefig(out, dpi = 300)
+    ax.grid(False)
+    ax.plot_surface(grid_r,grid_t,grid_e, rstride=1, cstride=1,
+        cmap='inferno')
+    plt.savefig(out, dpi = 200)
     print('Saved surface plot as ' + out)
 
 main()
