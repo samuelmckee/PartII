19c885beb55d136e9bfdd08e1ca8875a6a291e0f
Sam McKee <samuel.mckee98@gmail.com>
2018-10-12 21:42:25 +0100

    Added output file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
         dir = argv[1]
     except IndexError:
         print("Please enter directory as command line "
-                    "containing output files as argument.")
+            "containing output files as argument.")
         return # Exit program
 
     file_list = [str for str in listdir(dir) if str.endswith(".out")]
@@ -18,15 +18,37 @@ def main():
         return # Exit program
 
     data = [get_data(filename, dir) for filename
-                in file_list if get_data(filename, dir) != None]
+        in file_list if get_data(filename, dir) != None]
 
-    fit(data)
+    params = fit_data(data)
+    out = "out"
+
+    f = open(out + ".txt", "w")
+
+    f.write("Data read from %s \n\n\n" % dir)
+    f.write("Equilibrium geometry: \n\n")
+    f.write("Bond length = %f Angstroms\n" % params.get('r_eq'))
+    f.write("Bond angle = %f degrees\n" % params.get('t_eq'))
+    f.write("Energy = %f Hartrees\n\n\n" % params.get('e_eq'))
+    f.write("Vibrational frequencties:\n\n")
+    f.write("Frequency of symmetric stretch = %f cm^-1\n"
+        % params.get('nu_1'))
+    f.write("Symmetric stretch data fitted using %d data points with R^2 = %f\n"
+        % (params.get('r_fit')[2], params.get('r_fit')[1]))
+    f.write("\n")
+    f.write("Frequency of bending mode = %f cm^-1\n"
+        % params.get('nu_2'))
+    f.write(("Bending mode data fitted using %d data points with R^2 = %f\n")
+        % (params.get('t_fit')[2], params.get('t_fit')[1]))
+    f.close()
+    print("Wrote calculated parameters data to %s.txt" % out)
 
 
 # Functions for importing data from files
 def get_theta(filename):
     theta = filename.split("theta")[1][:-4]
     return float(theta)
+
 
 def get_r(filename):
     r = filename.split("theta")[0].split("r")[-1]
@@ -38,6 +60,7 @@ def get_energy(file_path):
     for line in f:
         if "SCF Done" in line:
             l = line.split()
+            f.close()
             return float(l[4])
 
 
@@ -52,6 +75,66 @@ def get_data(filename, dir):
 
 
 # Functions for processing data
+def fit_data(data):
+    # Get equilibrium geometry
+    r_eq, t_eq, e_eq = min(data, key = lambda tup: tup[2])
+
+    # Fit data along symmetric stretch
+    r_mode = [(r, t, e) for (r, t, e) in data if t == t_eq ]  # Keep t = t_eq
+    r_mode.sort(key = lambda tup: tup[0])
+    rs = [x[0] * 1.0e-10 for x in r_mode]      # Get r in m
+    es = [x[2] * 4.35974e-18 for x in r_mode]  # Get E in J
+    r_fit = fit_to_tol(rs, es, r_eq * 1.0e-10, 0.95)
+    k_r = 2. * r_fit[0]
+
+    # Fit data along bending mode
+    t_mode = [(r, t, e) for (r, t, e) in data if r == r_eq]  # Keep r = r_eq
+    t_mode.sort(key = lambda tup: tup[1])
+    ts = [x[1] * pi / 180. for x in t_mode]    # Get theta in radians
+    es = [x[2] * 4.35974e-18 for x in t_mode]  # Get E in J
+    t_fit = fit_to_tol(ts, es, t_eq * pi / 180., 0.95)
+    k_t = 2. * t_fit[0]
+
+    # Calculate vibrational frequencties in wavenumbers
+    mu_1 = 2.0 * 1.66e-27
+    mu_2 = 0.5 * 1.66e-27
+
+    nu_1 = sqrt(k_r / mu_1) / (2. * pi) * 33.35641e-12
+    nu_2 = sqrt(k_t / ((r_eq ** 2) * 1.0e-20 * mu_2)) / (2. * pi) * 33.35641e-12
+
+    return {'r_eq' : r_eq,
+            't_eq' : t_eq,
+            'e_eq' : e_eq,
+            'r_fit': r_fit,
+            't_fit': t_fit,
+            'k_r'  : k_r,
+            'k_t'  : k_t,
+            'nu_1' : nu_1,
+            'nu_2' : nu_2
+            }
+
+
+# Fits quadratic to xs and ys, reducing maximum displacement from equilibrium
+# Until R^2 rises above minimum value
+def fit_to_tol(xs, ys, x_eq, tol):
+    p = polyfit(xs, ys, 2) # Fit data to quadratic
+    range = max([abs(x-x_eq) for x in xs])
+    step = range / (2. * len(xs)) # Amount max displacement reduced
+                                  # by after each fit
+    R2 = get_r2(p, xs, ys)
+
+    while R2 < tol:     # R^2 below threshold
+        #Reduce maximum displacement from equilibrium
+        range -= step
+        ys = [y for (i, y) in enumerate(ys) if abs(xs[i] - x_eq) < range]
+        xs = [x for x in xs if abs(x - x_eq) < range]
+
+        #Recalculate fit
+        p = polyfit(xs, ys, 2)
+        R2 = get_r2(p,xs,ys)
+
+    return (p[0], R2, len(xs))
+
 
 # Returns r^2 value for polynomial p which is fitted to xs and ys
 def get_r2(p, xs, ys):
@@ -62,50 +145,5 @@ def get_r2(p, xs, ys):
     SSres = sum(y_res**2)               # Residual sum of squares
 
     return 1. - SSres / SStot
-
-# Fits quadratic to xs and ys, reducing maximum displacement from equilibrium
-# Until R^2 rises above minimum value
-def fit_to_tol(xs, ys, x_eq, r2):
-    p = polyfit(xs, ys, 2) # Fit data to quadratic
-    range = max([abs(x-x_eq) for x in xs])
-    step = range / 20. # Amount max displacement reduced by after each fit
-    R2 = get_r2(p, xs, ys)
-
-    while R2 < r2:     # R^2 below threshold
-        #Reduce maximum displacement from equilibrium
-        range -= step
-        ys = [y for (i, y) in enumerate(ys) if abs(xs[i] - x_eq) < range]
-        xs = [x for x in xs if abs(x - x_eq) < range]
-
-        #Recalculate fit
-        p = polyfit(xs, ys, 2)
-        R2 = get_r2(p,xs,ys)
-
-    print (len(xs))
-    return p[0]
-
-def fit(data):
-    r_eq, t_eq, e_eq = min(data, key = lambda tup: tup[2])
-
-    r_fit = [(r, t, e) for (r, t, e) in data if t == t_eq ]  # Keep t = t_eq
-    r_fit.sort(key = lambda tup: tup[0])
-    rs = [x[0] * 1.0e-10 for x in r_fit]      # Get r in m
-    es = [x[2] * 4.35974e-18 for x in r_fit]  # Get E in J
-    k_r = 2. * fit_to_tol(rs, es, r_eq * 1.0e-10, 0.95)
-
-    t_fit = [(r, t, e) for (r, t, e) in data if r == r_eq]  # Keep r = r_eq
-    t_fit.sort(key = lambda tup: tup[1])
-    ts = [x[1] * pi / 180. for x in t_fit]    # Get theta in radians
-    es = [x[2] * 4.35974e-18 for x in t_fit]  # Get E in J
-    k_t = 2. * fit_to_tol(ts, es, t_eq * pi / 180., 0.95)
-
-    mu_1 = 2.0 * 1.66e-27
-    mu_2 = 0.5 * 1.66e-27
-
-    nu_1 = sqrt(k_r / mu_1) / (2. * pi)
-    nu_2 = sqrt(k_t / ((r_eq ** 2) * 1.0e-20 * mu_2)) / (2. * pi)
-
-    print(nu_1 * 33.35e-12)
-    print(nu_2 * 33.35e-12)
 
 main()
