1b3e47d584cb5c4773c902fa8da33e072d3417f7
Sam McKee <samuel.mckee98@gmail.com>
2018-10-12 11:08:13 +0100

    Fit to given r^2

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def main():
     except IndexError:
         print("Please enter directory as command line "
                     "containing output files as argument.")
-        return #Exit program
+        return # Exit program
 
     file_list = [str for str in listdir(dir) if str.endswith(".out")]
 
     if len(file_list) == 0:
         print("No .out files found in given directory")
-        return #Exit program
+        return # Exit program
 
     data = [get_data(filename, dir) for filename
                 in file_list if get_data(filename, dir) != None]
@@ -48,37 +48,64 @@ def get_data(filename, dir):
         e = get_energy(dir + "/" + filename)
         return (r,t,e)
     except:
-        print("Failed to get data point for " + filename)
+        print("Failed to read data from " + filename)
 
 
-#Functions for processing data
+# Functions for processing data
+
+# Returns r^2 value for polynomial p which is fitted to xs and ys
+def get_r2(p, xs, ys):
+    y_fit = [polyval(p, x) for x in xs] # y values predicted by parameters
+    y_avg = sum(ys)/len(ys)             # Average y value
+    SStot = sum((ys - y_avg)**2)        # Sum of squares
+    y_res = subtract(ys, y_fit)         # Residuals
+    SSres = sum(y_res**2)               # Residual sum of squares
+
+    return 1. - SSres / SStot
+
+# Fits quadratic to xs and ys, reducing maximum displacement from equilibrium
+# Until R^2 rises above minimum value
+def fit_to_tol(xs, ys, x_eq, r2):
+    p = polyfit(xs, ys, 2) # Fit data to quadratic
+    range = max([abs(x-x_eq) for x in xs])
+    step = range / 20. # Amount max displacement reduced by after each fit
+    R2 = get_r2(p, xs, ys)
+
+    while R2 < r2:     # R^2 below threshold
+        #Reduce maximum displacement from equilibrium
+        range -= step
+        ys = [y for (i, y) in enumerate(ys) if abs(xs[i] - x_eq) < range]
+        xs = [x for x in xs if abs(x - x_eq) < range]
+
+        #Recalculate fit
+        p = polyfit(xs, ys, 2)
+        R2 = get_r2(p,xs,ys)
+
+    print (len(xs))
+    return p[0]
+
 def fit(data):
     r_eq, t_eq, e_eq = min(data, key = lambda tup: tup[2])
 
     r_fit = [(r, t, e) for (r, t, e) in data if t == t_eq ]  # Keep t = t_eq
     r_fit.sort(key = lambda tup: tup[0])
-    rs = [x[0] * 1.0e-10 for x in r_fit]      #Get r in m
-    es = [x[2] * 4.35974e-18 for x in r_fit]  #Get E in J
-    k_r = 2. * polyfit(rs,es,2)[0]
+    rs = [x[0] * 1.0e-10 for x in r_fit]      # Get r in m
+    es = [x[2] * 4.35974e-18 for x in r_fit]  # Get E in J
+    k_r = 2. * fit_to_tol(rs, es, r_eq * 1.0e-10, 0.95)
 
-    t_fit = [(r, t, e) for (r, t, e) in data if r == r_eq]  #Keep r = r_eq
+    t_fit = [(r, t, e) for (r, t, e) in data if r == r_eq]  # Keep r = r_eq
     t_fit.sort(key = lambda tup: tup[1])
-    ts = [x[1] * pi / 180. for x in t_fit]    #Get theta in radians
-    es = [x[2] * 4.35974e-18 for x in t_fit]  #Get E in J
-    k_t = 2. * polyfit(ts,es,2)[0]
+    ts = [x[1] * pi / 180. for x in t_fit]    # Get theta in radians
+    es = [x[2] * 4.35974e-18 for x in t_fit]  # Get E in J
+    k_t = 2. * fit_to_tol(ts, es, t_eq * pi / 180., 0.95)
 
     mu_1 = 2.0 * 1.66e-27
     mu_2 = 0.5 * 1.66e-27
 
     nu_1 = sqrt(k_r / mu_1) / (2. * pi)
-    nu_2 = sqrt(k_t / (r_eq * r_eq * 1.0e-20 * mu_2)) / (2. * pi)
+    nu_2 = sqrt(k_t / ((r_eq ** 2) * 1.0e-20 * mu_2)) / (2. * pi)
 
-    print(r_eq)
-    print(t_eq)
-    print(e_eq)
-    print(nu_1 * 33.5e-12)
-    print(nu_2 * 33.5e-12)
-
-
+    print(nu_1 * 33.35e-12)
+    print(nu_2 * 33.35e-12)
 
 main()
